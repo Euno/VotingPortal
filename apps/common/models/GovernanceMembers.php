5baabe54b8f27c7acbbc3c98d5e87ebbad351c35
arjanvanderwal <arjanvdwal1992@gmail.com>
2019-02-20 22:00:03 +0100

    -- Added signup module (backend)

---
+++ apps/common/models/GovernanceMembers.php
@@ -0,0 +1,133 @@
+<?php
+namespace EunoVoting\Common\Models;
+use EunoVoting\Common\Libraries\jsonRPCClient;
+use Phalcon\Mvc\Model\Behavior\SoftDelete;
+
+class GovernanceMembers extends \Phalcon\Mvc\Model
+{
+    /**
+     *
+     * @var integer
+     * @Primary
+     * @Identity
+     * @Column(type="integer", length=11, nullable=false)
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=45, nullable=false)
+     */
+    public $telegram;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=true)
+     */
+    public $discord;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=true)
+     */
+    public $twitter_handle;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", length=45, nullable=false)
+     */
+    public $masternode_address;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    public $masternode_ipaddress;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    public $signed_msg;
+
+    /**
+     *
+     * @var integer
+     * @Column(type="integer", nullable=false)
+     */
+    public $access;
+
+    /**
+     *
+     * @var integer
+     * @Column(type="integer", nullable=false)
+     */
+    public $date;
+
+    /**
+     *
+     * @var integer
+     * @Column(type="integer", nullable=false)
+     */
+    public $deleted;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'governance_members';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Admins[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Admins
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+    public function initialize()
+    {
+        $this->addBehavior(new softDelete([
+            "field" => "deleted",
+            "value" => 1
+        ]));
+    }
+
+    /**
+     * Check the signed message related to this model
+     *
+     * @return mixed
+     */
+    public function checkHash()
+    {
+        $config = $this->getDI()->get('config');
+        $connect_string = sprintf('http://%s:%s@%s:%s/', $config->eunod->user, $config->eunod->pass, $config->eunod->host, $config->eunod->port);
+        $coind = new jsonRPCClient($connect_string);
+
+        return $coind->verifymessage($this->masternode_address, $this->signed_msg, $this->telegram);
+    }
+}
